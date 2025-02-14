b99032deaab865be425e4c7b92195c3163bb6f97
Дедов Иван <johnnyded@icloud.com>
2025-02-14 23:37:12 +0300

    Initial commit

--- FirstMiniHomework/obj/Debug/net9.0/FirstMiniHomework.AssemblyInfo.cs
+++ FirstMiniHomework/obj/Debug/net9.0/FirstMiniHomework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstMiniHomework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3f5babeebe2a0fd741677ffb3e8f037aeaa7e0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstMiniHomework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstMiniHomework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
