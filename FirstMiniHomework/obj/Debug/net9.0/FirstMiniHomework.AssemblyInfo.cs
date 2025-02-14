a66745ebe3d5e47e6e1575d9fa70ccbbf2e60d51
Дедов Иван <johnnyded@icloud.com>
2025-02-15 01:34:20 +0300

    Delete unnecessary

--- FirstMiniHomework/obj/Debug/net9.0/FirstMiniHomework.AssemblyInfo.cs
+++ FirstMiniHomework/obj/Debug/net9.0/FirstMiniHomework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstMiniHomework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ecc23f70a4126194452a24cde7e45e09395f4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16fe2a49b972febf01a94f2863e90061e3479c0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstMiniHomework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstMiniHomework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
